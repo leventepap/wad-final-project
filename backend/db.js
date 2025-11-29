3d15ba0685a0a370103c0d4bbbcd48b44e243b58
Levente Pap <pap.levente.i@gmail.com>
2025-11-29 23:49:03 +0100

    linted backend files

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database('../db/library.db', (err) => {
+const db = new sqlite3.Database("../db/library.db", (err) => {
     if (err) {
-        console.error('Error opening database:', err.message);
+        console.error("Error opening database:", err.message);
     } else {
-        console.log('Successfully connected to the Library DB.');
+        console.log("Successfully connected to the Library DB.");
     }
 });
 
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const cors = require("cors");
 const PORT = 3001;
 
 app.use(express.json());
 
-const bookRoutes = require('./book');
+const bookRoutes = require("./book");
 
 app.use(cors());
-app.use('/api/books', bookRoutes);
+app.use("/api/books", bookRoutes);
 
 app.listen(PORT, () => {
     console.log(`Library backend server running on http://localhost:${PORT}`);
