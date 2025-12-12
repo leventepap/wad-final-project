de526fde3aa5f909e5f809f81cbe7bd991a68c21
Levente Pap <pap.levente.i@gmail.com>
2025-12-12 23:23:58 +0100

    disabled for react strict mode for development

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  //disabled for development
+  reactStrictMode: false,
 };
 
 export default nextConfig;
