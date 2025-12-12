f6596474f9a44bd725205209a5e327ede1edd8f4
Levente Pap <pap.levente.i@gmail.com>
2025-12-12 23:53:11 +0100

    fixed Button color bug

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -10,15 +10,15 @@ export default function Button(props: { label: string, onCLick: Function, color?
 
     let color;
     switch (props.color) {
-        case ButtonColor.PRIMARY || undefined: color = "indigo"; break;
-        case ButtonColor.INACTIVE: color = "gray"; break;
-        case ButtonColor.WARNING: color = "red"; break;
+        case ButtonColor.INACTIVE: color = "bg-gray-500 hover:bg-gray-600"; break;
+        case ButtonColor.WARNING: color = "bg-red-500 hover:bg-red-600"; break;
+        case ButtonColor.PRIMARY:
+        default: color = "bg-indigo-500 hover:bg-indigo-600"; break;
     }
-    const colorClass = `bg-${color}-500 hover:bg-${color}-600`;
 
     return (
         <button onClick={() => props.onCLick()}
-                className={`px-4 py-2 rounded-lg text-white shadow-md hover:shadow-lg focus:outline-none cursor-pointer mt-auto ${colorClass}`}>
+                className={`px-4 py-2 rounded-lg text-white shadow-md hover:shadow-lg focus:outline-none cursor-pointer mt-auto ${color}`}>
             {props.label}
         </button>
     );
